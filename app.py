95cb3161ddb5bbe5dc1ac9683a60673c49ef484f
DevockaKoder <56919331+DevockaKoder@users.noreply.github.com>
2022-11-03 00:46:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -84,26 +84,33 @@ label = np.argmax(y_test[0])
 print("Íîìåð êëàññà:", label)
 print("Íàçâàíèå êëàññà:", classes[label])
 
-#ñîçäàåì ôóíêöèþ çàãðóçêè ôàéëà
-
-def load_image():
-    uploaded_file = st.file_uploader(label='Âûáåðèòå èçîáðàæåíèå äëÿ ðàñïîçíàâàíèÿ')
-    if uploaded_file is not None:
-        image_data = uploaded_file.getvalue()
-        st.image(image_data)
-        return Image.open(io.BytesIO(image_data))
-    else:
-        return None
+#jfjjf
 
 def preprocess_image(img):
-    img = image.load_img(img_path, target_size=(28, 28), color_mode = "grayscale")
+    img = img.resize((224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
     return x
 
 
-st.title('Распознавание одежды на фото')
+def load_image():
+    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+    
+model = load_model()
+    
+def print_predictions(preds):
+    classes = decode_predictions(preds, top=3)[0]
+    for cl in classes:
+        st.write(str(cl[1]).replace('_'," "), cl[2])
+
+st.title('Распознавание изображений')
 img = load_image()
 result = st.button('Распознать изображение')
 if result:
