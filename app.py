7a21062258ea3dc3430b740a37328fc96c7381a2
DevockaKoder <56919331+DevockaKoder@users.noreply.github.com>
2022-11-03 00:50:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,21 +8,11 @@ import io
 import requests
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
 from PIL import Image
 
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
 classes = ['футболка', 'брюки', 'свитер', 'платье', 'пальто', 'туфли', 'рубашка', 'кроссовки', 'сумка', 'ботинки']
-
-plt.figure(figsize=(10,10))
-for i in range(100,150):
-    plt.subplot(5,10,i-100+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(x_train[i], cmap=plt.cm.binary)
-    plt.xlabel(classes[y_train[i]])
 
 x_train = x_train.reshape(60000, 784)
 x_test = x_test.reshape(10000, 784)
