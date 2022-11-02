7056039af5b9ac44083b228a48eec7984abe0677
DevockaKoder <56919331+DevockaKoder@users.noreply.github.com>
2022-11-03 00:49:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-%matplotlib inline 
 
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
